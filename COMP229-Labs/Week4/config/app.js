1d7b7c4936b5fe1c23175d12d19b3f134eeecd18
eaglespeaks <132870871+eaglespeaks@users.noreply.github.com>
2023-06-18 18:47:06 -0400

    New progress made

--- COMP229-Labs/Week4/config/app.js
+++ COMP229-Labs/Week4/config/app.js
@@ -4,24 +4,40 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+// database set up
+let mongoose = require('mongoose');
+let DB = require('./db');
+
+let indexRouter = require('../routes/index');
+let usersRouter = require('../routes/users');
+let gamesRouter = require('../routes/game');
+
+// point mongoose to the DB URI
+mongoose.connect(DB.URI);
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error: '));
+mongoDB.once('open', ()=>{
+  console.log('Connected to mongoDB...');
+})
+
 
 let app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/game-list', gamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ COMP229-Labs/Week4/config/db.ts
@@ -0,0 +1,2 @@
+export const LocalURI = "mongodb://localhost/game_store"
+export const RemoteURI = "mongodb+srv://eaglespeaks:BEgW7Uayg2SvQlfv@eaglespeaks.a8lrh2w.mongodb.net/"
